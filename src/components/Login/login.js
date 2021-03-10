28e7e29cee08683ce175f55969742277c1afd2b2
Sheldon Monteiro <sheldonmonteiro10@gmail.com>
2021-03-10 23:18:17 +0530

    Fourth commit

--- src/components/Login/login.js
+++ src/components/Login/login.js
@@ -71,23 +71,96 @@ function Login() {
   );
 }
 
-const LoginForm = () => {
-  const history = useHistory();
-  
-  const Submit = (event) => {
-    event.preventDefault();
-    history.push("/home");
+class LoginForm extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      fields: {},
+      errors: {}
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.submituserLoginForm = this.submituserLoginForm.bind(this);
   };
+
+  handleChange(event) {
+    let fields = this.state.fields;
+    fields[event.target.name] = event.target.value;
+    this.setState({
+      fields
+    });
+
+  }
+
+  submituserLoginForm(event) {
+    event.preventDefault();
+    if (this.validateForm()) {
+        let fields = {};
+        fields["username"] = "";
+        fields["password"] = "";
+        this.setState({fields:fields});
+        window.open("/home", "_self");
+    }
+
+  }
+
+  validateForm() {
+    let fields = this.state.fields;
+    let errors = {};
+    let formIsValid = true;
+
+    if (!fields["username"]) {
+      formIsValid = false;
+      errors["username"] = "*Please enter your username.";
+    }
+
+    if (typeof fields["username"] !== "undefined") {
+      if (!fields["username"].match(/^[a-zA-Z ]*$/)) {
+        formIsValid = false;
+        errors["username"] = "*Please enter alphabet characters only.";
+      }
+    }
+
+    if (!fields["password"]) {
+      formIsValid = false;
+      errors["password"] = "*Please enter your password.";
+    }
+
+    if (typeof fields["password"] !== "undefined") {
+      if (fields["password"].length < 6) {
+        formIsValid = false;
+        errors["password"] = "*Please enter secure and strong password.";
+      }
+    }
+
+    this.setState({
+      errors: errors
+    });
+
+    return formIsValid;
+  }
   
-  return (
-    <form>
-      <label htmlFor="username" >username</label>
-      <input type="username" id="username" placeholder="Please enter your username"  />
-      <label htmlFor="password" >password</label>
-      <input type="password" id="password" placeholder="Please enter your password" />
-      <input type="submit" className="submit" value="Sign In" onClick={Submit}/>
-    </form>
-  ); 
+  render(){
+    return (
+      <form action="" className="form" onClick={this.submituserLoginForm} >
+        <label htmlFor="username" >username</label>
+        <input type="username" 
+        id="username" 
+        placeholder="Please enter your username" 
+        value={this.state.fields.username} 
+        onChange={this.handleChange} />
+        <div className="errormsg">{this.state.errors.username}</div>
+        <label htmlFor="password" >password</label>
+        <input type="password" 
+        id="password" 
+        placeholder="Please enter your password" 
+        value={this.state.fields.password} 
+        onChange={this.handleChange}/>
+        <div className="errormsg">{this.state.errors.password}</div>
+        <input type="submit" className="submit" value="Sign In" />
+      </form>
+   ); 
+  }
 }
 
 class RegisterForm extends React.Component {
@@ -97,13 +170,9 @@ class RegisterForm extends React.Component {
       fields: {},
       errors: {}
     };
-
-    
-    
+ 
     this.handleChange = this.handleChange.bind(this);
     this.submituserRegistrationForm = this.submituserRegistrationForm.bind(this);
-    
-
   };
   
   handleChange(event) {
@@ -207,7 +276,7 @@ class RegisterForm extends React.Component {
         name="username" 
         placeholder="Please enter your username" 
         value={this.state.fields.username} 
-        onChange={this.handleChange}   />
+        onChange={this.handleChange} />
         <div className="errormsg">{this.state.errors.username}</div>
         <label htmlFor="email">email</label>
         <input 

2f21af050e6dc27cdc021b74c5190cf5805b2300
Sheldon Monteiro <sheldonmonteiro10@gmail.com>
2021-03-27 23:59:21 +0530

    19th commit

--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -1,5 +1,13 @@
 import React, {useState} from 'react';
 import Select from 'react-select';
+import {
+    Accordion,
+    AccordionItem,
+    AccordionItemHeading,
+    AccordionItemButton,
+    AccordionItemPanel,
+} from 'react-accessible-accordion';
+import 'react-accessible-accordion/dist/fancy-example.css';
 import "./home.css";
 import Navbar from "../Navbar/navbar";
 import cold from "./pictures/cold.png";
@@ -7,6 +15,7 @@ import migraine from "./pictures/migraine.png";
 import accupressure from "./pictures/accupressure.png";
 import conjunctivitis from "./pictures/conjunctivitis.png";
 import diarrhea from "./pictures/diarrhea.png";
+import covid from "./pictures/covid.png"
 
 
 function HomePage() {
@@ -18,6 +27,7 @@ function HomePage() {
         {label : "Sneezing", value:4, category: "cat-2"},
         {label : "Body-pain", value:5, category: "cat-1"},
         {label : "Cough", value:6, category: "cat-2"},
+        {label : "Tiredness", value:7, category: "cat-1"},
         {label : "Throbbing/Frequent headache", value:7, category: "cat-1"},
         {label : "Pain on one side of the head", value:8, category: "cat-1"},
         {label : "Pain in the forehead", value:9, category: "cat-1"},
@@ -46,6 +56,8 @@ function HomePage() {
         {label : "Mucus in the stool", value:32, category: "cat-1"},
         {label : "Bloating", value:33, category: "cat-1"},
         
+        
+        
     ];
     
 
@@ -65,36 +77,91 @@ function HomePage() {
             case ("Fever,Runny-nose,Body-pain,Cough"): case ("Fever,Runny-nose,Cough"):
                 setDisplay (
                 <div className="window">
-                    <p>
-                        <h2>Common Cold:</h2>
-                         A common viral infection of the nose and throat.In contrast to the flu, a common cold can be caused by many different types of viruses. 
-                         The condition is generally harmless and symptoms usually resolve within two weeks.
-                    </p>
-                    <img src={cold} />
-                    <p>
-                        <h2>Home Remedies:</h2>
-                        <div className="space"/>
-                        <h4>Warm baths:</h4> You can reduce a child’s fever by giving them a warm sponge bath. 
-                        Warm baths can also reduce cold and flu symptoms in adults. 
-                        Adding Epsom salt and baking soda to the water can reduce body aches.
-                        Adding a few drops of essential oil, such as tea tree, juniper, rosemary, thyme, orange, lavender, or eucalyptus, may also have a soothing effect.
+                    <Accordion allowMultipleExpanded allowZeroExpanded>
+                        <AccordionItem>
+                            <AccordionItemHeading>
+                                <AccordionItemButton> Common cold </AccordionItemButton>
+                            </AccordionItemHeading >
+                            <AccordionItemPanel>
+                                <p>
+                                    <h2>Common Cold:</h2>
+                                    A common viral infection of the nose and throat.In contrast to the flu, a common cold can be caused by many different types of viruses. 
+                                    The condition is generally harmless and symptoms usually resolve within two weeks.
+                                </p>
+                                <img src={cold} />
+                                <p>
+                                    <h2>Home Remedies:</h2>
+                                    <div className="space"/>
+                                    <h4>Warm baths:</h4> You can reduce a child’s fever by giving them a warm sponge bath. 
+                                    Warm baths can also reduce cold and flu symptoms in adults. 
+                                    Adding Epsom salt and baking soda to the water can reduce body aches.
+                                    Adding a few drops of essential oil, such as tea tree, juniper, rosemary, thyme, orange, lavender, or eucalyptus, may also have a soothing effect.
                         
-                        <div className="space"/>
+                                    <div className="space"/>
                         
-                        <h4>Ginger:</h4>A few slices of raw ginger root in boiling water may help soothe a cough or sore throat. 
-                        Research suggests that it can also ward off the feelings of nausea that so often accompany influenza.
+                                    <h4>Ginger:</h4>A few slices of raw ginger root in boiling water may help soothe a cough or sore throat. 
+                                    Research suggests that it can also ward off the feelings of nausea that so often accompany influenza.
 
-                        <div className="space"/>
+                                    <div className="space"/>
 
-                        <h4>Honey:</h4>Drinking honey in tea with lemon can ease sore throat pain. 
-                            Research suggests that honey is an effective cough suppressant, too.
+                                    <h4>Honey:</h4>Drinking honey in tea with lemon can ease sore throat pain. 
+                                    Research suggests that honey is an effective cough suppressant, too.
 
-                        <div className="space"/>
+                                    <div className="space"/>
 
-                        <h4>Salt water:</h4>Gargling with salt water reduces and loosens mucus, which contains bacteria and allergens.
-                         To try this remedy at home, dissolve 1 teaspoon of salt in a full glass of water.
-                          Swish it around your mouth and throat. Then spit it out.
-                    </p>
+                                    <h4>Salt water:</h4>Gargling with salt water reduces and loosens mucus, which contains bacteria and allergens.
+                                    To try this remedy at home, dissolve 1 teaspoon of salt in a full glass of water.
+                                    Swish it around your mouth and throat. Then spit it out.
+                                </p>
+                            </AccordionItemPanel>
+                        </AccordionItem>
+                        <AccordionItem>
+                            <AccordionItemHeading>
+                                <AccordionItemButton>COVID-19 (Coronavirus)</AccordionItemButton>
+                            </AccordionItemHeading>
+                            <AccordionItemPanel>
+                                <p>
+                                <h2>COVID-19 (Coronavirus):</h2>
+                                Coronavirus disease (COVID-19) is an infectious disease caused by a newly discovered coronavirus.
+                                Most people infected with the COVID-19 virus will experience mild to moderate respiratory illness and recover without requiring special treatment.  
+                                Older people, and those with underlying medical problems like cardiovascular disease, diabetes, chronic respiratory disease, and cancer are more likely to develop serious illness.
+
+                                <br/><br/>The virus is now known as the severe acute respiratory syndrome coronavirus 2 (SARS-CoV-2). 
+                                The disease it causes is called coronavirus disease 2019 (COVID-19).
+                                In March 2020, the World Health Organization (WHO) declared the COVID-19 outbreak a pandemic.
+
+                                <br/><br/><b>Less common symptoms:</b>
+                                <br/>[1]aches and pains 
+                                <br/>[2]sore throat
+                                <br/>[3]diarrhea
+                                <br/>[4]conjunctivitis
+                                <br/>[5]headache
+                                <br/>[6]loss of taste or smell
+                                <br/>[7]a rash on skin
+                                <br/>[8]discolouration of fingers or toes
+
+                                <br/><br/><b>Serious symptoms:</b>
+                                <br/>[1]difficulty breathing or shortness of breath 
+                                <br/>[2]chest pain or pressure
+                                <br/>[3]loss of speech or movement
+                                
+                                </p>
+                                <img src={covid} />
+                                <p>
+                                    <h5 className="note">
+                                        *Please note that the following remedies are only meant to alleviate mild to moderate symptoms. 
+                                        In case of serious symptoms, kindly contact your doctor immediately and maintain social-distancing*
+                                    </h5>
+                                    <div className="space"/>
+                                    <h2>Home Remedies:</h2>
+                                    <div className="space"/>
+                                    
+                                    <h4>Cool It Down:</h4>
+
+                                </p>
+                            </AccordionItemPanel>
+            </AccordionItem>
+        </Accordion>
                 </div>
                 );
 
@@ -172,7 +239,7 @@ function HomePage() {
                         Using a cool and damp compress will help reduce inflammation and soothe the eye.
                         <br/><br/>How to use a compress for pinkeye:
                         <br/>[1] soak a clean washcloth or hand towel in warm or cool water
-                        <br/>[4] wring out any excess water
+                        <br/>[2] wring out any excess water
                         <br/>[3] place the damp washcloth over the eye and leave there for a few minutes
                         <br/>[4] remove the washcloth from the eye and wash your hands immediately
                         
@@ -218,7 +285,46 @@ function HomePage() {
                     <p>
                         <h2>Home Remedies:</h2>
                         <div className="space"/>
-                        <h4>Use a cool/warm compress:</h4>
+                        <h4>Rehydrating:</h4>
+                        Hydrating the body is essential to recovering from diarrhea.Diarrhea causes a deficit of fluids, including water. 
+                        This causes the body to lose electrolytes such as sodium and chloride.To support recovery, it is vital to restore fluids. 
+                        Otherwise, a person may become dehydrated. <br/><br/>Drinking water is the first step to rehydrating. 
+                        A person can also to create an oral rehydration solution by mixing 1 liter of water with half a teaspoon of salt and 6 teaspoons of sugar. 
+                        Consuming sugar and salt with water helps the intestines to absorb fluids more efficiently. 
+                        This solution more effectively rehydrates the body after a bout of diarrhea than water alone. 
+                        Other drinks can also be beneficial. For example, drinking sports drinks can help rehydrate the body and restore potassium and sodium. 
+                        Fruit juices can also help restore potassium.
+
+                        <br/><br/>Avoid drinking anything that will further irritate the digestive tract, such as: caffeinated drinks, alcohol, 
+                        carbonated beverages and very hot drinks
+
+                        <div className="space"/>
+
+                        <h4>BRAT diet:</h4>
+                        BRAT stands for Bananas, Rice, Applesauce, and Toast. 
+                        These foods are all starchy, so they can help bind foods together to make stools firmer. 
+                        This may decrease the number of stools a person passes and help ease their diarrhea. 
+                        As these foods are bland, they do not contain substances that irritate the stomach, throat, or intestines. 
+                        <br/><br/>Therefore, this diet can soothe the tissue irritation resulting from the acids in vomit. 
+                        Many of the foods in the BRAT diet are also high in nutrients such as potassium and magnesium and can replace those lost through diarrhea and vomiting.
+
+                        <div className="space"/>
+
+                        <h4>Coconut water:</h4>
+                        Coconut water contains high levels of potassium and magnesium. These nutrients help to reduce pain, muscle spasms, and cramps. 
+                        Coconut water is also useful for rehydrating and is a better option than most sports drinks as it is also low in calories, sugar, and acidity. 
+                        Slowly sipping on up to 2 glasses of coconut water every 4–6 hours could ease upset stomach symptoms.
+
+                        <div className="space"/>
+
+                        <h4>Taking probiotics:</h4>
+                        Probiotics are microorganisms that can benefit the digestive system. 
+                        They can support the workings of the gut and help fight off infection. 
+                        Probiotics are live bacteria and yeasts in some yogurts and other fermented foods.
+                        In 2010, researchers conducted a large systematic review of 63 studies on probiotics, with over 8,000 participants. 
+                        They found that probiotics significantly shortened the duration of recovery from diarrhea.
+
+                        <br/><br/> Foods that are rich in probiotics are: Yogurt, Buttermilk, Soft and Aged Cheeses, Kimchi.
                     </p>
 
                 </div>
@@ -226,6 +332,16 @@ function HomePage() {
                 );
 
                 break;
+
+            case (""):
+                setDisplay(
+                    <div className="window">
+                        
+                    </div>
+
+                );
+
+            break;
       
             default:
                 setDisplay( <div className="window">kaput</div>);
